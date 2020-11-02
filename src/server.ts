6292733ebf6335b703af298022ad92b5808701d8
Lincoln Gadea <lincoln@glinc.com.br>
2020-11-02 15:21:33 -0300

    Primeiro Commit Estrutural

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+/**
+ * importa o express para do typescript
+ * Sempre que usarmos typescript, quando usamos o express precisamos adicionar
+ * a biblioteca do expres do typescript
+ *
+ * comando: yarn add @types/express
+ */
+import express from 'express';
+import routes from './routes';
+
+//Cria a aplicação para conexão com o server na porta especificada
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333,()=>{
+    console.log('🚀 Server Started on port 3333');
+});
