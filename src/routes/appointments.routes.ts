f2dbffdb748fe670a95212db71ba7dd60cdd5e72
Lincoln Gadea <lincoln@glinc.com.br>
2020-11-02 16:34:00 -0300

    Cria a automaçao de appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,40 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import {startOfHour, parseISO, isEqual} from 'date-fns';
 
 const appointmentRouter = Router();
+
+interface Appointment{
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appoitments: Appointment[] = [];
+
+/** Exemplo do redirecionamento de roda criado no server ts apontando para o appointmentRouter */
+appointmentRouter.post('/',(request, response)=>{
+    const {provider, date} = request.body;
+
+    const parseDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appoitments.find(appoitment=>isEqual(parseDate,appoitment.date));
+
+    if(findAppointmentInSameDate){
+        return response
+            .status(400)
+            .json({message:"This appointment is already booked"});
+    }
+
+    const appoitment = {
+        id:uuid(),
+        provider,
+        date: parseDate,
+    }
+
+    appoitments.push(appoitment);
+
+    return response.json(appoitment);
+})
 
 export default appointmentRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,14 @@
 // src/routes/index.ts
 import { Router } from 'express';
+import appointmentRouter from  './appointments.routes';
 
 const routes = Router();
 
+/**
+ * O routes.use para o appointements, garante que sempre que na url, houver o /appointments,
+ * essa fota será direcionada para o appointments.routes.ts, onde poderá ser tratada
+ * a partir de '/' como se fosse a raiz do projeto, poŕem iniciando a partir do appointments.
+ */
+routes.use('/appointments', appointmentRouter);
 
-// routes.get('/',(request,response)=>{
-//     response.json({message: "Hello World"});
-// });
-
-    routes.post('/users',(request, response)=>{
-        const {name, email} = request.body;
-        const user = {
-            name,
-            email
-        };
-
-        return response.json(user);
-    });
 export default routes;
