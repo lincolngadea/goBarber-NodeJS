d729178dd3222a76dfb04ced46a790bb771fad8d
Lincoln Gadea <lincoln@glinc.com.br>
2020-11-11 19:47:37 -0300

    Tratativa de erros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,7 +8,7 @@ const routes = Router();
 
 /**
  * O routes.use para o appointements, garante que sempre que na url, houver o /appointments,
- * essa fota será direcionada para o appointments.routes.ts, onde poderá ser tratada
+ * essa  fota será direcionada para o appointments.routes.ts, onde poderá ser tratada
  * a partir de '/' como se fosse a raiz do projeto, poŕem iniciando a partir do appointments.
  */
 routes.use('/appointments', appointmentsRouter);
