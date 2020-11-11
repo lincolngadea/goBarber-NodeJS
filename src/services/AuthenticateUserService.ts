99ed1b186f04a488b42ace538c5b8a6b2d235f8e
Lincoln Gadea <lincoln@glinc.com.br>
2020-11-11 13:45:18 -0300

    Cria codificaçao de exceptions

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -5,6 +5,8 @@ import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 interface Request {
     email: string;
     password: string;
@@ -22,7 +24,7 @@ class AuthenticateUserService {
         const user = await usersRepository.findOne({ where: { email } });
 
         if (!user) {
-            throw new Error('Incorrect email/password combination.');
+            throw new AppError('Incorrect email/password combination.', 401);
         }
 
         // user.password - Senha não criptografada
@@ -31,7 +33,7 @@ class AuthenticateUserService {
         const passwordMatched = await compare(password, user.password);
 
         if (!passwordMatched) {
-            throw new Error('Incorrect email/password combination.');
+            throw new AppError('Incorrect email/password combination.', 401);
         }
 
         const { secret, expiresIn } = authConfig.jwt;
